1afa788765b1eefa495d6661d715d2a5e78249f4
魏仁杰 <weijosephrj@gmail.com>
2019-08-02 20:21:33 +0800

    @eternalimmortal

--- TCN/depression_by_face/depression_test.py
+++ TCN/depression_by_face/depression_test.py
@@ -13,33 +13,35 @@ import numpy as np
 from TCN.depression_by_face.model import TCN
 from TCN.depression_by_face.utils import trainset,testset
 
-parser = argparse.ArgumentParser(description='Sequence Modeling - depression with 2D')
+parser = argparse.ArgumentParser(description='Sequence Modeling - depression by face')
+parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                    help='batch size (default: 64)')
 parser.add_argument('--cuda', action='store_false',
                     help='use CUDA (default: True)')
-parser.add_argument('--dropout', type=float, default=0.25,
-                    help='dropout applied to layers (default: 0.25)')
-parser.add_argument('--clip', type=float, default=0.2,
-                    help='gradient clip, -1 means no clip (default: 0.2)')
-parser.add_argument('--epochs', type=int, default=100,
-                    help='upper epoch limit (default: 100)')
-parser.add_argument('--ksize', type=int, default=5,
-                    help='kernel size (default: 5)')
-parser.add_argument('--levels', type=int, default=4,
-                    help='# of levels (default: 4)')
+parser.add_argument('--dropout', type=float, default=0.05,
+                    help='dropout applied to layers (default: 0.05)')
+parser.add_argument('--clip', type=float, default=-1,
+                    help='gradient clip, -1 means no clip (default: -1)')
+parser.add_argument('--epochs', type=int, default=20,
+                    help='upper epoch limit (default: 20)')
+parser.add_argument('--ksize', type=int, default=7,
+                    help='kernel size (default: 7)')
+parser.add_argument('--levels', type=int, default=8,
+                    help='# of levels (default: 8)')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='report interval (default: 100')
-parser.add_argument('--lr', type=float, default=1e-3,
-                    help='initial learning rate (default: 1e-3)')
+parser.add_argument('--lr', type=float, default=2e-3,
+                    help='initial learning rate (default: 2e-3)')
 parser.add_argument('--optim', type=str, default='Adam',
                     help='optimizer to use (default: Adam)')
-parser.add_argument('--nhid', type=int, default=150,
-                    help='number of hidden units per layer (default: 150)')
-parser.add_argument('--data', type=str, default='Nott',
-                    help='the dataset to run (default: Nott)')
+parser.add_argument('--nhid', type=int, default=25,
+                    help='number of hidden units per layer (default: 25)')
 parser.add_argument('--seed', type=int, default=1111,
                     help='random seed (default: 1111)')
-
+parser.add_argument('--permute', action='store_true',
+                    help='use permuted MNIST (default: false)')
 args = parser.parse_args()
+
 
 # Set the random seed manually for reproducibility.
 torch.manual_seed(args.seed)
@@ -100,34 +102,34 @@ def train(ep):
             train_loss = 0
 
 
-def test():
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            if args.cuda:
-                data, target = data.cuda(), target.cuda()
-            data = data.view(-1, input_channels, seq_length)
-            if args.permute:
-                data = data[:, :, permute]
-            data, target = Variable(data, volatile=True), Variable(target)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-
-        test_loss /= len(test_loader.dataset)
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-        return test_loss
+# def test():
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             if args.cuda:
+#                 data, target = data.cuda(), target.cuda()
+#             data = data.view(-1, input_channels, seq_length)
+#             if args.permute:
+#                 data = data[:, :, permute]
+#             data, target = Variable(data, volatile=True), Variable(target)
+#             output = model(data)
+#             test_loss += F.nll_loss(output, target, size_average=False).item()
+#             pred = output.data.max(1, keepdim=True)[1]
+#             correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+#
+#         test_loss /= len(test_loader.dataset)
+#         print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#             test_loss, correct, len(test_loader.dataset),
+#             100. * correct / len(test_loader.dataset)))
+#         return test_loss
 
 
 if __name__ == "__main__":
     for epoch in range(1, epochs+1):
         train(epoch)
-        test()
+        # test()
         if epoch % 10 == 0:
             lr /= 10
             for param_group in optimizer.param_groups:
